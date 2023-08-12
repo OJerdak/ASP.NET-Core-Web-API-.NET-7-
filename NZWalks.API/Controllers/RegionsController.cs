cb524db068834c305f2527d989ee60ec0d919cc9
ojerdak-koein <ojerdak@koein.com>
2023-08-12 21:36:56 +0300

    Add GetById Action

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -21,7 +21,33 @@ namespace NZWalks.API.Controllers
         {
             var regions = dbContext.Regions.ToList();
 
+            if(regions == null)
+            {
+                return NotFound();
+            }
+
             return Ok(regions);
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //Find Method can only be used with primary key
+
+            var region = dbContext.Regions.Find(id);
+
+            //or using First Or Default LINQ Method
+            //var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+
+
     }
 }
