c5a30a3b086a5c4848d166c07971d66d3ebf2813
ojerdak-koein <ojerdak@koein.com>
2023-08-27 23:04:30 +0300

    Add Register Functionality

---
+++ NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await _userManager.CreateAsync(identityUser , registerRequestDto.Password);
+
+            if(identityResult.Succeeded)
+            {
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await _userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+                    
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("User was successfully registered! Please login.");
+                    }
+                }
+            }
+
+            return BadRequest("Something went wrong");
+
+        }
+    }
+}
