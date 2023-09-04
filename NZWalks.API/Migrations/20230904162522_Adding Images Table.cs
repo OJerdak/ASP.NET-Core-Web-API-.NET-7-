076835fe0fb27bf0b61180d1eaf86c357abaeed3
ojerdak-koein <ojerdak@koein.com>
2023-09-04 20:00:40 +0300

    Add Image Upload Model And DTO

---
+++ NZWalks.API/Migrations/20230904162522_Adding Images Table.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingImagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_Regions_RegionID",
+                table: "Walks");
+
+            migrationBuilder.RenameColumn(
+                name: "RegionID",
+                table: "Walks",
+                newName: "RegionId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Walks_RegionID",
+                table: "Walks",
+                newName: "IX_Walks_RegionId");
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileExtension = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileSizeInBytes = table.Column<long>(type: "bigint", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_Regions_RegionId",
+                table: "Walks",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_Regions_RegionId",
+                table: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.RenameColumn(
+                name: "RegionId",
+                table: "Walks",
+                newName: "RegionID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Walks_RegionId",
+                table: "Walks",
+                newName: "IX_Walks_RegionID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_Regions_RegionID",
+                table: "Walks",
+                column: "RegionID",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
