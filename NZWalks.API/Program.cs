bf8bde782fd118bfc155e4c86393cb5bfe02f611
ojerdak-koein <ojerdak@koein.com>
2023-09-04 22:51:22 +0300

    Enable Static Files Serving

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -7,6 +7,7 @@ using NZWalks.API.Repositories;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -102,6 +103,12 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseStaticFiles( new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory,"Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
