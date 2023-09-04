217b4f2a2d86b398bd6a61fbe86ec751f2bc873a
ojerdak-koein <ojerdak@koein.com>
2023-09-04 20:01:30 +0300

    Add Images Controller

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+
+            if(ModelState.IsValid)
+            {
+                //Use image upload repo
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if(!allowedExtensions.Contains(Path.GetExtension(request.FileName))) 
+            {
+                ModelState.AddModelError("file", "Unsupported File Format");
+            }
+
+            if(request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10MB, please upload a smaller size file.");
+            }
+        }
+    }
+}
